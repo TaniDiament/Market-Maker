451097a147ca062524eaaad58b5e6b185ec6561e
yydiamen <tanidiament@gmail.com>
2026-05-12 17:36:04 -0400

    UI

---
+++ src/main/java/edu/yu/marketmaker/state/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package edu.yu.marketmaker.state;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
+
+@Configuration
+@Profile("trading-state")
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
